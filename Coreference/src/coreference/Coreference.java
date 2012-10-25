7ed80195d3db4f7dc0108fd525de899288035e88
Gavin Earley <gavinearley@gmail.com>
2012-10-24 20:20:35 -0600

    Added file writer method
    Because I'm Batman!

--- Coreference/src/coreference/Coreference.java
+++ Coreference/src/coreference/Coreference.java
@@ -1,6 +1,8 @@
 package coreference;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * 
@@ -13,6 +15,8 @@ public class Coreference {
 	/**
 	 * @param args
 	 */
+	public static String directory;
+	
 	public static void main(String[] args) {
 		//grab cmd line arguement
 		if(args.length !=2 ) {
@@ -20,15 +24,21 @@ public class Coreference {
 			  System.exit(1);
 			}
 
-		int i=0;
+
 		String inputList= args[0]; //input list file
-		String directory =args[1]; //directory for output files
+		 directory =args[1]; //directory for output files
 
 		try
 		{
 			//read in input file to get files to process
 			ReadFile listFile = new ReadFile(inputList);
 			String[] listArray = listFile.OpenFile();
+			
+			//loop through our list and process each file
+			for(int i=0; i<listArray.length; i++)
+			{
+				processFile(listArray[i]);
+			}
 			
 		}
 		catch (IOException e )
@@ -37,6 +47,34 @@ public class Coreference {
 			
 		}
 		
+	}
+	
+	public static void processFile(String file)
+	{
+		//open the file and read each line
+		try
+		{
+			//read each line of current file
+			ReadFile currentFile = new ReadFile(file);
+			String[] currentFileArray = currentFile.OpenFile();
+		}
+		catch (IOException e )
+		{
+			System.out.println(e.getMessage());
+			
+		}
+		
+		//while looping through each line do what we need to do
+	}
+	
+	public static void printOutput(Tag tag, int fileID) throws Exception{
+		
+		PrintWriter pw = new PrintWriter(new FileWriter(directory+fileID+".response"));
+	   
+	      pw.print("<COREF ID=");
+	     
+	    pw.println();
+	    pw.close(); 
 	}
 
 }
